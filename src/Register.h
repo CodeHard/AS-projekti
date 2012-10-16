905bd670ee23ba8e3e3438a41412990ea024b66b
Eero Laukkanen <elaukkane@gmail.com>
2012-10-16 15:47:03 +0300

    initial registration procedure

--- src/Register.h
+++ src/Register.h
@@ -1,21 +1,70 @@
 #pragma once
+#include <iostream>
 #include <pcl/point_cloud.h>
+#include <pcl/registration/ndt.h>
+#include <pcl/filters/approximate_voxel_grid.h>
 
 namespace askinect {
 
   template<typename T>
   class Register {
     private:
-      pcl::PointCloud<T> previous_cloud;
+      pcl::PointCloud<T> previousCloud;
+      bool isInitialized;
 
     public:
-      Register() {}
+      Register() : isInitialized(false) {}
       ~Register() {}
 
-      const pcl::PointCloud<T> &registerNew(const pcl::PointCloud<T> &new_cloud) {
-        pcl::PointCloud<T> cld;
-        previous_cloud = cld;
-        return previous_cloud;
+      const pcl::PointCloud<T> &registerNew(const pcl::PointCloud<T> &newCloud) {
+
+        if (isInitialized) {
+		  boost::shared_ptr<const pcl::PointCloud<T> > newCloudPtr(&newCloud);
+		  boost::shared_ptr<const pcl::PointCloud<T> > previousCloudPtr(&previousCloud);
+
+          pcl::PointCloud<T>::Ptr filtered_cloud (new pcl::PointCloud<T>);
+          pcl::ApproximateVoxelGrid<T> approximate_voxel_filter;
+          approximate_voxel_filter.setLeafSize (0.2, 0.2, 0.2);
+		  
+          approximate_voxel_filter.setInputCloud (newCloudPtr);
+          approximate_voxel_filter.filter (*filtered_cloud);
+
+          pcl::NormalDistributionsTransform<T, T> ndt;
+          // Setting scale dependent NDT parameters
+          // Setting minimum transformation difference for termination condition.
+          ndt.setTransformationEpsilon (0.01);
+          // Setting maximum step size for More-Thuente line search.
+          ndt.setStepSize (0.1);
+          //Setting Resolution of NDT grid structure (VoxelGridCovariance).
+          ndt.setResolution (1.0);
+          // Setting max number of registration iterations.
+          ndt.setMaximumIterations (35);
+          // Setting point cloud to be aligned.
+          ndt.setInputCloud (filtered_cloud);
+          // Setting point cloud to be aligned to.
+          ndt.setInputTarget (previousCloudPtr);
+
+          // Calculating required rigid transform to align the input cloud to the target cloud.
+          pcl::PointCloud<T>::Ptr output_cloud (new pcl::PointCloud<T>);
+		  ndt.align (*output_cloud, Eigen::Matrix4f::Identity());
+
+          std::cout << "Normal Distributions Transform has converged:" << ndt.hasConverged ()
+                    << " score: " << ndt.getFitnessScore () << std::endl;
+
+          // Transforming unfiltered, input cloud using found transform.
+          pcl::transformPointCloud (newCloud, *output_cloud, ndt.getFinalTransformation ());
+
+          previousCloud = *output_cloud;
+
+        } else {
+
+          previousCloud = newCloud;
+          isInitialized = true;
+
+        }
+
+        return previousCloud;
+
       }
   };
 
